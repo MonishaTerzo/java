eda95165086eeef314e7f00de4793414497bc872
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 13:22:51 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+        String a = " Hi i am monishaa";
+        //endswith
+        System.out.println(a.endsWith("monisha"));
 
+        //startswith
+        System.out.println(a.startsWith("i"));
+
+        //only 15 letters allowed
+        int len = a.length();
+        if (len > 15) {
+            System.out.println("please fill the form within 15 letter");
         }
+
+        //find the index where the name starts
+        int index = a.indexOf("m");
+
+        ///change the content
+        a.replace("i am", "my name is");
+
+        //lowercase and uppercase
+        System.out.println(a.toLowerCase());
+        System.out.println(a.toUpperCase());
+
+        //trim
+        System.out.println(a.trim());
+
+        //charAt
+        System.out.println(a.charAt(1));
+
+        //empty or not
+        System.out.println(a.isEmpty());
+
+
     }
 }
