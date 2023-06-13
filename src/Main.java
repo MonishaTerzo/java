b9f406ddd36fc733feaea4e1ccf66f15f464ab75
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 17:22:41 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-            NumberFormat a= NumberFormat.getCurrencyInstance();
-            String g=a.format(768756764);
-        System.out.println(g);
-        NumberFormat h= NumberFormat.getPercentInstance();
-        String ii=h.format(1.0);
-        System.out.println(ii);
-
+        int mark1 = 99;
+        int mark2 = 77;
+        String gh = mark1 > mark2 ? "mark1 is good" : "mark 2 is good";
+        System.out.println(gh);
 
 
     }
-    }
+}
