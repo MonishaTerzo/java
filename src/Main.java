61b9286c976003d037b104f8815e9253ed65cba5
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 22:41:58 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         boolean an=true;
+        int principal=0;
         while(an){
         System.out.print("enter the anount you want between $1000 to $1,00000:");
-
-        int principal=sc.nextInt();
+        principal=sc.nextInt();
         if(principal<1000 || principal>1000000){
             System.out.println("enter valid value");
             }
@@ -31,9 +31,9 @@ public class Main {
         System.out.print("enter the duration");
         int years=sc.nextInt();
         long total_payment=years*12;
-        double k=Math.pow(1+monthly_interest,years);
+        double k=Math.pow(1+monthly_interest,total_payment);
         double total_amount=0.0f;
-        total_amount=(monthly_interest*k)/k-1;
+        total_amount=(principal*k)/k-1;
         System.out.println("your final amount"+"$"+total_amount);
         NumberFormat a= NumberFormat.getCurrencyInstance();
         String g=a.format(total_amount);
