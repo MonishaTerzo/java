e666373f2e197cb2e4f068bd8cbddeb4486db432
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 22:51:35 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
 import java.text.NumberFormat;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-            NumberFormat a= NumberFormat.getCurrencyInstance();
-            String g=a.format(768756764);
-        System.out.println(g);
-        NumberFormat h= NumberFormat.getPercentInstance();
-        String ii=h.format(1.0);
-        System.out.println(ii);
+        {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter the number: ");
+                int n = sc.nextInt();
+                System.out.println("fizz or Buzz, 0r FizzBuzz are: ");
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i%3==0 && i%5==0)
+                    {
+
+                        System.out.println(i+" - FizzBuzz");
+                    }
+
+                    else if (i%3==0)
+                    {
+                        System.out.println(i+ " - Fizz");
+                    }
+
+                    else if (i%5==0)
+                    {
+                        System.out.println(i + "- Buzz");
+                    }
+                    else
+                    {
+                        System.out.println(i);
+                    }
+
+            }
+        }}}
 
 
 
-    }
-    }
+
+
