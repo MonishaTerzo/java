1202fd8bfcecc9d6b0e850b33b26f642b4e8b6df
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 14:33:57 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-            int a=80;
-
+        String greeting="hi \"moni\"";
+        String myfolder="c:\\myfolder";
+        String namelist="monisha\nmosh\nharini";
+        String namealign="i\tam\ta\tterzo\temployeee";
+        System.out.println(greeting);
+        System.out.println(myfolder);
+        System.out.println(namelist);
+        System.out.println(namealign);
         }
     }
-}
