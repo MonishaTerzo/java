3bbb892cca6c8824d84c5ecf4964c699441e3d99
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 12:45:22 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+        //array
+        int[] a = {1, 2};
+        a[1] = 5;
+        System.out.println(Arrays.toString(a));
+        //date
+        Date b=new Date();
+        System.out.println(b.getTime());
 
-        }
     }
 }
