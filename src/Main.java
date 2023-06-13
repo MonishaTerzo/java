9e0b0c80e3e30ce483c878d71278af3996f801ca
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 22:18:25 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 import java.text.NumberFormat;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-            NumberFormat a= NumberFormat.getCurrencyInstance();
-            String g=a.format(768756764);
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the anount you want:");
+        int principal=sc.nextInt();
+        System.out.println("enter the interest rate:") ;
+        float interest=sc.nextFloat();
+        float monthly_interest=interest/100/12;
+        System.out.println("enter the duration");
+        int years=sc.nextInt();
+        long total_payment=years*12;
+        double k=Math.pow(1+monthly_interest,years);
+        double total_amount=0.0f;
+        total_amount=(monthly_interest*k)/k-1;
+        System.out.println("your final amount"+"$"+total_amount);
+        NumberFormat a= NumberFormat.getCurrencyInstance();
+        String g=a.format(total_amount);
         System.out.println(g);
-        NumberFormat h= NumberFormat.getPercentInstance();
-        String ii=h.format(1.0);
-        System.out.println(ii);
-
-
 
     }
     }
