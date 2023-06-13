5abbf7f5dd16e658abb35a68632f9745a67c0b93
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 16:35:48 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 import java.text.NumberFormat;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-            NumberFormat a= NumberFormat.getCurrencyInstance();
-            String g=a.format(768756764);
+        Scanner sc=new Scanner(System.in);
+        byte a=sc.nextByte();
+        short b=sc.nextShort();
+        int c=sc.nextInt();
+        float d=sc.nextFloat();
+        double e=sc.nextDouble();
+        long f=sc.nextLong();
+        char g=sc.next().charAt(0);
+        String h=sc.next();
+        String k=sc.nextLine();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
         System.out.println(g);
-        NumberFormat h= NumberFormat.getPercentInstance();
-        String ii=h.format(1.0);
-        System.out.println(ii);
+        System.out.println(h);
+        System.out.println(k);
 
 
 
