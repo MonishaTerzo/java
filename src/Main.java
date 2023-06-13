fe1704854e167fe4caf8231bd65fe5505b580c59
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 17:02:01 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 import java.text.NumberFormat;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-            NumberFormat a= NumberFormat.getCurrencyInstance();
-            String g=a.format(768756764);
-        System.out.println(g);
-        NumberFormat h= NumberFormat.getPercentInstance();
-        String ii=h.format(1.0);
-        System.out.println(ii);
+        Scanner sc=new Scanner(System.in);
+        float temp=sc.nextFloat();
+        if(temp>30){
+            System.out.println("it is a hot day......drink more water");}
+        else if(temp>20 && temp<30){
+            System.out.println("it is normal day...dont upset be cool");}
+        else{
+            System.out.println("it is a cold day");}
 
 
 
