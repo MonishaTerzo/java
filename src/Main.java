3c391c79fdd571925b5b27b144547dbff144435c
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 16:20:51 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+            NumberFormat a= NumberFormat.getCurrencyInstance();
+            String g=a.format(768756764);
+        System.out.println(g);
+        NumberFormat h= NumberFormat.getPercentInstance();
+        String ii=h.format(1.0);
+        System.out.println(ii);
 
-        }
+
+
     }
-}
+    }
