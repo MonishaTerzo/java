36c0754ac7ad51ddf5d636faf7c80061d04417af
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 11:31:51 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.net.SocketOption;
+
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+        int a = 80;
+        System.out.println(a);
+        int input1 = 20, input2 = 45;
+        System.out.println(input1 + input2);
+        float var = 9.00f;
+        int v;
+        v = (int) var;
+        System.out.println(v);
 
-        }
+
     }
 }
