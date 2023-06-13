ac46292ee0530e12604c66d15fd883c19be4c706
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 15:21:26 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+        short salary=18_000;
+        short increment=1000;
+        int sum =salary+increment;
+        int sub=salary-increment;
+        int mul=salary*increment;
+        int div=salary/increment;
+        int a=8;
+        int b=9;
+        //modulus
+        System.out.println(a%b);
+        System.out.println(b++);
+        System.out.println(++b);
+        System.out.println(b--);
+        System.out.println(--b);
+        System.out.println(a/b);
+        float c= (float)a/(float)b;
+        System.out.println(a+=2);
+        System.out.println(a-=2);
+
+
 
         }
     }
-}
