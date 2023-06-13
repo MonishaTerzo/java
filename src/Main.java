ab260949a18eb3692b3666447f58936ab9151cce
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 15:10:30 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-            int a=80;
-
+        public static class company {
+            final static int rating = 5;//final variable
         }
-    }
-}
+            public static void main( String  args[]) {
+                company obj = new company();
+            System.out.println(obj.rating);
+            }
+        }
