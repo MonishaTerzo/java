1f5c7e6335241897d2a190fc4699d39222dd12a5
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 14:52:10 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+        ArrayList<Integer> a=new ArrayList<Integer>();
+        a.add(2);
+        a.add(3);
+        a.add(9);
+        System.out.println(a);
+        System.out.println(a.size());
+        a.remove(2);
+        System.out.println(a);
 
-        }
+     }
     }
-}
