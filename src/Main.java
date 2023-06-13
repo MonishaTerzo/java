ef521e9996a118ba74f8ba9546ea809f298ad924
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 16:57:27 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,25 @@ import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-            NumberFormat a= NumberFormat.getCurrencyInstance();
-            String g=a.format(768756764);
-        System.out.println(g);
-        NumberFormat h= NumberFormat.getPercentInstance();
-        String ii=h.format(1.0);
-        System.out.println(ii);
 
-
+           int rating =6;
+           int performance=10;
+           boolean character=false;
+           boolean increment=rating>5 && performance>8;
+           if(increment==true){
+               System.out.println("u have increment");}
+           else{
+               System.out.println("there is no increment");}
+           int test1mark=4;
+           int test2mark=9;
+           if(test1mark>5 || test2mark>5){
+               System.out.println("super u done the test well");}
+           else{
+               System.out.println("do well");}
+           boolean overall_performance= (rating>5 || test1mark>5) && !character;
+           if(overall_performance){
+               System.out.println("you are a great person");
+           }
 
     }
     }
