5cbd3da67d6afac9406cbcf033d5a83458b51ad8
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 15:25:32 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+            int a=80000;
+            int b=9;
+            int c=4;
+            long ans=a*76*b+c-8;
+        System.out.println(ans);
+        long ans2=a*76*(b+c-8);
+        System.out.println(ans2);
 
-        }
+
     }
-}
+    }
