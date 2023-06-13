fde037f8e4f6f95bc5f2a8585a8382b5c9b2e371
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 12:04:45 +0530

    primitive

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+          byte mark=88;
+          short bank_balace=28_000;
+          int company_count=3_777_666;
+          long no_of_people=3_5555_5555_555_9999L;
+          float celsius=21.8f;
+          double c=5.897987798798;
+          boolean terzo_is_a_good_company=true;
+          char initial='R';
+
+
 
         }
     }
-}
