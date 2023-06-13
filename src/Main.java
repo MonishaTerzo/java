9f052c32d0804020e62d83df674551338491b2fe
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 21:38:03 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,10 @@ import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-            NumberFormat a= NumberFormat.getCurrencyInstance();
-            String g=a.format(768756764);
-        System.out.println(g);
-        NumberFormat h= NumberFormat.getPercentInstance();
-        String ii=h.format(1.0);
-        System.out.println(ii);
-
+      int[] a={1,2,3};
+      for(int i:a){
+          System.out.println(i);
 
 
     }
-    }
+    }}
