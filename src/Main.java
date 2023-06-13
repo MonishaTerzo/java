36624c19c0bd1674a0e43602a83688f3d58dae1e
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 12:56:23 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+        //primitive
+        int a = 20;
+        int b = a;
+        a = 14;
+        System.out.println(b);
 
-        }
+        //non_primitive
+        int[] n = {1, 2};
+        n[0] = 9;
+        System.out.println(n[0]);
     }
 }
