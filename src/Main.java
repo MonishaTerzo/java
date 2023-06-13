389e8d8f71e9fca049aecd86c818d40aa5cd2103
MonishaTerzo <monisha@terzocloud.com>
2023-06-13 15:01:20 +0530

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-            int a=80;
+        int[][] a = new int[3][2];
+        int h = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                h=h+1;
+                a[i][j] = h ;
+            }}
+            System.out.println(Arrays.deepToString(a));
+            int[][] b = {{8, 6, 55}, {6, 7, 9}};
+            System.out.println(b[0][0]);
 
         }
     }
-}
